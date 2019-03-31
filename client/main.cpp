3cb2e6c21be32265de246517ce1e1e0f5b40622f
Ehsan <Ehsan@samani.com>
2019-03-31 15:48:17 -0500

    conenct from tcp client

--- client/main.cpp
+++ client/main.cpp
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
+#include <arpa/inet.h>
 
 using namespace std; 
+extern int errno;
+int main(int argc, char *argv[]){
+  if(argc < 3) {
+    cout<<"command line arguments needed for client to connect to server {ip} {port}."<<endl;
+    cout<<"example:./client 127.0.0.1 1099"<<endl;
+    return 1;
+  }
+  //ipv6 maximum plus null terminator
+  char szAddr[46] = {0}; 
+  strcpy(&szAddr[0], argv[1]);
 
-int main(){
+  int nPort = atoi(argv[2]);
+  unsigned short maxPort = 0xFFFF;
+  if(nPort > maxPort) {
+    cout<<"Port:"<<nPort<<" is larget than maximum allowed "<<maxPort<<"."<<endl;
+    return 2;
+  }
+  unsigned short port = nPort;
+
   int clientSocket;
-  char buffer[1024];
   struct sockaddr_in serverAddr;
-  socklen_t addr_size;
 
   /*---- Create the socket. The three arguments are: ----*/
   /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
@@ -20,21 +37,26 @@ int main(){
   /* Address family = Internet */
   serverAddr.sin_family = AF_INET;
   /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(7891);
+  serverAddr.sin_port = htons(port);
   /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serverAddr.sin_addr.s_addr = inet_addr(szAddr);
   /* Set all bits of the padding field to 0 */
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
   /*---- Connect the socket to the server using the address struct ----*/
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+  socklen_t addr_size = sizeof serverAddr;
+  int nCode = connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+  if (nCode == 0 ) {
+    char buffer[1024];
+    /*---- Read the message from the server into the buffer ----*/
+    recv(clientSocket, buffer, 1024, 0);
 
-  /*---- Read the message from the server into the buffer ----*/
-  recv(clientSocket, buffer, 1024, 0);
+    /*---- Print the received message ----*/
+    printf("Data received: %s",buffer);   
 
-  /*---- Print the received message ----*/
-  printf("Data received: %s",buffer);   
-
-  return 0;
+    return 0;
+  } else {
+       char * pszErrorString = strerror(errno);
+       cout<<"Eror Connecting:"<<pszErrorString<<endl;
+  }
 }
--- server/main.cpp
+++ server/main.cpp
@@ -8,7 +8,8 @@
 
 using namespace std; 
 
-int main(){
+int main(int argc, char *argv[]){
+
   int welcomeSocket, newSocket;
   char buffer[1024];
   struct sockaddr_in serverAddr;
